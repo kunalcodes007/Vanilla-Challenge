36aaf29fad3daf7557279e44011eb01c25e2cf76
kunalcodes007 <kunallingwal2@gmail.com>
2022-09-05 12:49:36 +0530

    Initial Commit

---
+++ 1/Drum-kit/script.js
@@ -0,0 +1,15 @@
+window.document.addEventListener("keydown", function (e) {
+  const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
+  const key = document.querySelector(`.key[data-key="${e.keyCode}"]`);
+  if (!audio) return;
+  audio.currentTime = 0;
+  audio.play();
+  function removeTransition(e) {
+    this.classList.remove("playing");
+  }
+  key.classList.add("playing");
+  const keys = document.querySelectorAll(".key");
+  keys.forEach((key) =>
+    key.addEventListener("transitionend", removeTransition)
+  );
+});
